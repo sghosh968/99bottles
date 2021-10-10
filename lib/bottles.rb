4be2d77037ea45cd05c1329b342719df03fd93eb
Suman Ghosh <sghosh968@gmail.com>
2021-10-11 00:54:32 +0530

    finished the problem - yay all tests green now

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,14 +1,22 @@
 class Bottles
-  def verse(num)
+  def verse num
     current = num
-    after = num - 1
+    after = current.zero? ? 99 : current - 1;
 
-    bottles_before = if current === 0
-      "no more bottles"
+    first_line = if current.zero?
+      "No more bottles of beer on the wall, no more bottles of beer."
     elsif current === 1
-      "1 bottle"
+      "1 bottle of beer on the wall, 1 bottle of beer."
     else
-      "#{current} bottles"
+      "#{current} bottles of beer on the wall, #{current} bottles of beer."
+    end
+
+    second_line = if after.zero?
+      "Take it down and pass it around, no more bottles of beer on the wall."
+    elsif after == 99
+      "Go to the store and buy some more, #{after} bottles of beer on the wall."
+    else
+      "Take one down and pass it around, #{after} #{after === 1 ? "bottle" : "bottles"} of beer on the wall."
     end
 
     bottles_after = if after === 0
@@ -19,8 +27,26 @@ class Bottles
       "#{after} bottles"
     end
     <<-VERSE
-#{bottles_before} of beer on the wall, #{bottles_before} of beer.
-Take #{current === 1 ? "it" : "one" } down and pass it around, #{bottles_after} of beer on the wall.
+#{first_line}
+#{second_line}
 VERSE
   end
+
+  def verses(start, finish)
+    num = start
+    results = []
+
+    while num >= finish
+      results << verse(num)
+      num -= 1
+    end
+
+    results.join("\n")
+  end
+
+  def song
+    verses(99, 0)
+  end
 end
+
+Bottles.new.verses(99, 98)
